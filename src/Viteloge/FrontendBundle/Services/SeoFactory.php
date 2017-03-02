fda86c99717aeed9048ca22e3fadd25040c9f07e
fmronan <fmronan@msn.com>
2017-03-02 13:59:37 +0100

    noref:canonicalseo use genereseo

--- src/Viteloge/FrontendBundle/Services/SeoFactory.php
+++ src/Viteloge/FrontendBundle/Services/SeoFactory.php
@@ -21,24 +21,7 @@ namespace Viteloge\FrontendBundle\Services {
 
         public function genereCanonicalSeo($index,$title,$description,$canonicalLink,$ogdescription=null,$ogtitle= null)
         {
-
-            $this->seoPage
-                ->setTitle($title)
-                ->addMeta('name', 'robots', $index)
-                ->addMeta('name', 'description', $description);
-                if(is_null($ogtitle)){
-                  $this->seoPage->addMeta('property', 'og:title', $this->seoPage->getTitle());
-                }else{
-                  $this->seoPage->addMeta('property', 'og:title', $ogtitle);
-                }
-                $this->seoPage->addMeta('property', 'og:type', 'website')
-                        ->addMeta('property', 'og:url',  $canonicalLink);
-                if(is_null($ogdescription)){
-                  $this->seoPage->addMeta('property', 'og:description', $description);
-                }else{
-                  $this->seoPage->addMeta('property', 'og:description', $ogdescription);
-                }
-
+              $this->seoPage =  $this->genereSeo($index,$title,$description,$canonicalLink,$ogdescription,$ogtitle);
                 $this->seoPage->setLinkCanonical($canonicalLink)
             ;
 
