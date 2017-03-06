c9569f65e41b2a9aa1e9b64d5c3a2a1cc146ee2a
fmronan <fmronan@msn.com>
2017-03-06 17:25:23 +0100

    noref:rem duplicate content with entities

--- src/Viteloge/CoreBundle/Entity/Base.php
+++ src/Viteloge/CoreBundle/Entity/Base.php
@@ -3,6 +3,8 @@
 namespace Viteloge\CoreBundle\Entity {
 
     use Doctrine\ORM\Mapping as ORM;
+    use Symfony\Component\Validator\Constraints as Assert;
+    use Viteloge\CoreBundle\Entity\User as CoreUser;
 
     class Base
     {
@@ -57,6 +59,59 @@ namespace Viteloge\CoreBundle\Entity {
          */
         protected $ua;
 
+        /**
+         *
+         */
+        protected $user;
+
+        /**
+         * @Assert\NotBlank()
+         * @Assert\Length(
+         *      min = "2",
+         *      max = "64"
+         * )
+         */
+        protected $firstname;
+
+        /**
+         * @Assert\NotBlank()
+         * @Assert\Length(
+         *      min = "2",
+         *      max = "64"
+         * )
+         */
+        protected $lastname;
+
+        /**
+         * @Assert\NotBlank()
+         * @Assert\Email(
+         *      checkHost = true,
+         *      checkMX = true
+         * )
+         */
+        protected $email;
+
+        /**
+         * @Assert\NotBlank()
+         * @Assert\Length(
+         *      min = "5",
+         *      max = "250"
+         * )
+         */
+        protected $message;
+
+        /**
+         * @Assert\Count(
+         *      min = "1",
+         *      max = "10",
+         *      minMessage = "assert.count.recommand.validate.min",
+         *      maxMessage = "assert.count.recommand.validate.max"
+         * )
+         * @Assert\All(
+         *     @Assert\Email()
+         * )
+         */
+        protected $emails;
 
         /**
          * Get date
@@ -207,6 +262,115 @@ namespace Viteloge\CoreBundle\Entity {
             return $this->ua;
         }
 
+        /**
+         *
+         */
+        public function getUser() {
+            return $this->user;
+        }
+
+        /**
+         *
+         */
+        public function setUser(CoreUser $user = null) {
+            if ($user instanceof CoreUser) {
+                $this->email = $this->user->getEmail();
+                $this->lastname = $this->user->getLastname();
+                $this->firstname = $this->user->getFirstname();
+            }
+            return $this;
+        }
+
+        /**
+         *
+         */
+        public function getFirstname() {
+            return $this->firstname;
+        }
+
+        /**
+         *
+         */
+        public function setFirstname($firstname) {
+            $this->firstname = $firstname;
+            return $this;
+        }
+
+        /**
+         *
+         */
+        public function getLastname() {
+            return $this->lastname;
+        }
+
+        /**
+         *
+         */
+        public function setLastname($lastname) {
+            $this->lastname = $lastname;
+            return $this;
+        }
+
+        /**
+         *
+         */
+        public function getFullname() {
+            return trim($this->getFirstname().' '.$this->getLastname());
+        }
+
+        /**
+         *
+         */
+        public function getEmail() {
+            return $this->email;
+        }
+
+        /**
+         *
+         */
+        public function setEmail($email) {
+            $this->email = $email;
+            return $this;
+        }
+
+        /**
+         *
+         */
+        public function getMessage() {
+            return $this->message;
+        }
+
+        /**
+         *
+         */
+        public function setMessage($message) {
+            $this->message = $message;
+            return $this;
+        }
+
+        /**
+         * Add email
+         *
+         */
+        public function addEmail($email) {
+            $this->emails[] = $email;
+
+            return $this;
+        }
+
+        /**
+         * Remove email
+         */
+        public function removeEmail($email) {
+            $this->emails->removeElement($email);
+        }
+
+        /**
+         * @return \Doctrine\Common\Collections\Collection
+         */
+        public function getEmails() {
+            return $this->emails;
+        }
 
     }
 
--- src/Viteloge/CoreBundle/Entity/Messagerie.php
+++ src/Viteloge/CoreBundle/Entity/Messagerie.php
@@ -4,7 +4,6 @@ namespace Viteloge\CoreBundle\Entity {
 
     use Doctrine\ORM\Mapping as ORM;
     use Symfony\Component\Validator\Constraints as Assert;
-    use Viteloge\CoreBundle\Entity\User as CoreUser;
     use Viteloge\CoreBundle\Entity\Base;
 
 
@@ -18,54 +17,6 @@ namespace Viteloge\CoreBundle\Entity {
          * @ORM\GeneratedValue(strategy="AUTO")
          */
         protected $id;
-
-        /**
-         *
-         * @ORM\ManyToOne(targetEntity="Viteloge\CoreBundle\Entity\User")
-         * @ORM\JoinColumns({
-         *   @ORM\JoinColumn(name="id", referencedColumnName="id")
-         * })
-         */
-        protected $user;
-
-        /**
-         * @Assert\Length(
-         *      min = "2",
-         *      max = "64"
-         * )
-         * @ORM\Column(name="firstname",type="string",length=255)
-         */
-        protected $firstname;
-
-        /**
-         * @Assert\NotBlank()
-         * @Assert\Length(
-         *      min = "2",
-         *      max = "64"
-         * )
-         * @ORM\Column(name="lastname",type="string",length=255)
-         */
-        protected $lastname;
-
-        /**
-         * @Assert\NotBlank()
-         * @Assert\Email(
-         *      checkHost = true,
-         *      checkMX = true
-         * )
-         * @ORM\Column(name="email",type="string",length=255, nullable=true)
-         */
-        protected $email;
-
-        /**
-         * @Assert\NotBlank()
-         * @Assert\Length(
-         *      min = "5",
-         *      max = "250"
-         * )
-         * @ORM\Column(name="message",type="string",length=255)
-         */
-        protected $message;
 
         /**
          * @Assert\Regex(
@@ -83,95 +34,6 @@ namespace Viteloge\CoreBundle\Entity {
          */
         protected function updateCreatedAt() {
             $this->date->setDate($this->getYear(), $this->getMonth(), $this->getDay());
-            return $this;
-        }
-
-
-        /**
-         *
-         */
-        public function getUser() {
-            return $this->user;
-        }
-
-        /**
-         *
-         */
-        public function setUser(CoreUser $user = null) {
-            if ($user instanceof CoreUser) {
-                $this->email = $user->getEmail();
-                $this->lastname = $user->getLastname();
-                $this->firstname = $user->getFirstname();
-                $this->phone = $user->getPhone();
-            }
-            return $this;
-        }
-
-        /**
-         *
-         */
-        public function getFirstname() {
-            return $this->firstname;
-        }
-
-        /**
-         *
-         */
-        public function setFirstname($firstname) {
-            $this->firstname = $firstname;
-            return $this;
-        }
-
-        /**
-         *
-         */
-        public function getLastname() {
-            return $this->lastname;
-        }
-
-        /**
-         *
-         */
-        public function setLastname($lastname) {
-            $this->lastname = $lastname;
-            return $this;
-        }
-
-        /**
-         *
-         */
-        public function getFullname() {
-            return trim($this->getFirstname().' '.$this->getLastname());
-        }
-
-
-        /**
-         *
-         */
-        public function getEmail() {
-            return $this->email;
-        }
-
-        /**
-         *
-         */
-        public function setEmail($email) {
-            $this->email = $email;
-            return $this;
-        }
-
-        /**
-         *
-         */
-        public function getMessage() {
-            return $this->message;
-        }
-
-        /**
-         *
-         */
-        public function setMessage($message) {
-            $this->message = $message;
             return $this;
         }
 
